7e954b5f099d197d0a699471c08b4e52f992c028
delver <dima.plugaturevchik@gmail.com>
2023-05-17 00:47:33 +0300

    Made 3 task

--- pw-3/src/index.ts
+++ pw-3/src/index.ts
@@ -1,9 +1,34 @@
 // import * as t1 from './1-task';
-import * as t2 from './2-task';
+// import * as t2 from './2-task';
+import * as t3 from './3-task';
 
 // TASK 1
 // @ts-ignore
 // console.log(t1.add(4)(6)());
 
 // TASK 2
-console.log(t2.stringsAreAnagrams('hello', 'olleh'));
+// console.log(t2.stringsAreAnagrams('hello', 'olleh'));
+
+// TASK 3
+type Bag = {
+	color: string;
+	weightKg: number;
+};
+
+type Student = {
+	age: number;
+	subjects: string[];
+	bag: Bag;
+};
+
+const student: Student = {
+	age: 40,
+	subjects: ['Math', 'Data science'],
+	bag: {
+		color: 'green',
+		weightKg: 5,
+	},
+};
+
+const clonedStudent: Student = t3.deepClone(student);
+console.log(JSON.stringify(student) === JSON.stringify(clonedStudent));
