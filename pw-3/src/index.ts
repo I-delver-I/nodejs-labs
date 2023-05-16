67abf225db9b8201aa41a140eeb39401a69a330a
delver <dima.plugaturevchik@gmail.com>
2023-05-16 23:41:53 +0300

    Made 2 task

--- pw-3/src/index.ts
+++ pw-3/src/index.ts
@@ -1,5 +1,9 @@
-import {add} from './1-task';
+// import * as t1 from './1-task';
+import * as t2 from './2-task';
 
 // TASK 1
 // @ts-ignore
-console.log(add(4)(6)());
+// console.log(t1.add(4)(6)());
+
+// TASK 2
+console.log(t2.stringsAreAnagrams('hello', 'olleh'));
