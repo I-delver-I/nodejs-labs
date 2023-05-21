1fa547ad22c774d249879b20973fb3d35f0ede1b
delver <dima.plugaturevchik@gmail.com>
2023-05-21 23:53:31 +0300

    Updated .eslintrc for treating a paragraph as spaces

--- pw-3/src/2-task.ts
+++ pw-3/src/2-task.ts
@@ -2,6 +2,7 @@ function stringsAreAnagrams(string1: string, string2: string): boolean {
   if (string1.length !== string2.length) {
     return false;
   }
+
   const sortedString1 = string1.split('').sort().join('');
   const sortedString2 = string2.split('').sort().join('');
 
